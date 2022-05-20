5479bc96d50ee7674ae53817ee0e47fff9104e55
padmams9999 <padmams9999@gmail.com>
2022-05-21 00:22:54 +0530

    first commit

---
+++ Framework_Backup01/src/test/java/Scenario_component/test.java
@@ -0,0 +1,12 @@
+package Scenario_component;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi Padma");
+
+	}
+
+}
